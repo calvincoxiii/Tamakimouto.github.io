5e261a129e75bbabc78fd066983f0a318f2d7fe3
Anthony Zheng <tamakimouto@gmail.com>
2015-08-01 13:30:25 -0400

    bgm test

--- js/bgw.js
+++ js/bgw.js
@@ -18,36 +18,38 @@ function timeControl(){
     if (time < 04){
         imgNum = "8";
         $('.sub').attr('data-value', "You should be sleeping right now");
-        $('.sub').attr('color', 'rgba(231, 227, 227, 1)');
-        $('.about').attr('color', 'rgba(37, 89, 101, 1');
+        $('.sub').css('color', 'rgba(231, 227, 227, 1)');
+        $('.about').css('color', 'rgba(37, 89, 101, 1');
+        $('.hilight').css('color', 'rgba(231, 227, 227, 1');
     } else if (time < 07) {
         imgNum = "1";
-        $('.sub').attr('data-value', "Good Morning, Visitor");
-        $('.sub').attr('color', 'rgba(231, 227, 227, 1)');
-        $('.about').attr('color', 'rgba(231, 227, 227, 1');
+        $('.sub').attr('data-value', "It's too early to be up right now, don't you think?");
+        $('.sub').css('color', 'rgba(231, 227, 227, 1)');
+        $('.about').css('color', 'rgba(231, 227, 227, 1');
     } else if (time < 10) {
         imgNum = "2";
-        $('.sub').attr('data-value', "You should be sleeping right now");
+        $('.sub').attr('data-value', "Good Morning, Visitor");
         $('.sub').attr('color', 'rgba(231, 227, 227, 1)');
         $('.about').attr('color', 'rgba(231, 227, 227, 1');
     } else if (time < 15) {
         imgNum = "3";
-        $('.sub').attr('data-value', "You should be sleeping right now");
-        $('.sub').attr('color', 'rgba(231, 227, 227, 1)');
-        $('.about').attr('color', 'rgba(231, 227, 227, 1');
+        $('.sub').attr('data-value', "Good Day, Visitor");
+        $('.sub').css('color', 'rgba(39, 72, 215, 1)');
+        $('.about').css('color', 'rgba(231, 227, 227, 1');
+        $('.hilight').css('color', 'rgba(23, 42, 128, 1)');
     } else if (time < 18) {
         imgNum = "4";
-        $('.sub').attr('data-value', "You should be sleeping right now");
+        $('.sub').attr('data-value', "Good Afternoon, Visitor");
         $('.sub').attr('color', 'rgba(231, 227, 227, 1)');
         $('.about').attr('color', 'rgba(231, 227, 227, 1');
     } else if (time < 19) {
         imgNum = "5";
-        $('.sub').attr('data-value', "You should be sleeping right now");
+        $('.sub').attr('data-value', "It's getting late isn't it, Visitor?");
         $('.sub').attr('color', 'rgba(231, 227, 227, 1)');
         $('.about').attr('color', 'rgba(231, 227, 227, 1');
     } else if (time < 20) {
         imgNum = "6";
-        $('.sub').attr('data-value', "You should be sleeping right now");
+        $('.sub').attr('data-value', "Good Evening, Visitor");
         $('.sub').attr('color', 'rgba(231, 227, 227, 1)');
         $('.about').attr('color', 'rgba(231, 227, 227, 1');
     } else {
